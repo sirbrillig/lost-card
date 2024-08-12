6b3561ff47cbe3a319053aaf9f9c78be67616980
Payton Swick <payton@foolord.com>
2024-08-11 20:27:47 -0400

    Fix GameOver scene

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -3,7 +3,6 @@ import { Scene } from "phaser";
 export class GameOver extends Scene {
 	camera: Phaser.Cameras.Scene2D.Camera;
 	background: Phaser.GameObjects.Image;
-	gameover_text: Phaser.GameObjects.Text;
 
 	constructor() {
 		super("GameOver");
@@ -13,14 +12,18 @@ export class GameOver extends Scene {
 		this.camera = this.cameras.main;
 		this.camera.setBackgroundColor(0x00000);
 
-		this.gameover_text = this.add.text(680, 384, "Game Over", {
+		const screenCenterX =
+			this.cameras.main.worldView.x + this.cameras.main.width / 2;
+		const screenCenterY =
+			this.cameras.main.worldView.y + this.cameras.main.height / 2;
+		const text = this.add.text(screenCenterX, screenCenterY, "Game Over", {
 			fontFamily: "Arial Black",
-			fontSize: 64,
+			fontSize: 24,
 			color: "#ffffff",
 			stroke: "#000000",
 			strokeThickness: 8,
 			align: "center",
 		});
-		this.gameover_text.setOrigin(0.5);
+		text.setOrigin(0.5);
 	}
 }
