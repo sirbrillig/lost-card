ef7fc696a3c95403cf52652466f1527b8bb8bd89
Payton Swick <payton@foolord.com>
2024-09-01 12:20:27 -0400

    Make loader bar fit on small screens

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -7,18 +7,18 @@ export class Preloader extends Scene {
 
 	init() {
 		//  We loaded this image in our Boot Scene, so we can display it here
-		this.add.image(512, 384, "background");
+		this.add.image(0, 0, "background");
 
 		//  A simple progress bar. This is the outline of the bar.
-		this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+		this.add.rectangle(0, 0, 200, 32).setStrokeStyle(1, 0xffffff);
 
 		//  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-		const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+		const bar = this.add.rectangle(0, 0, 4, 28, 0xffffff);
 
 		//  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
 		this.load.on("progress", (progress: number) => {
-			//  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-			bar.width = 4 + 460 * progress;
+			//  Update the progress bar (our bar is 200 wide, so 100% = 200)
+			bar.width = 4 + 200 * progress;
 		});
 	}
 
